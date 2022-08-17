afa976ee9e1a3959129c2a30103d67cd97e41178
Ayush Kumar <ayushmrityunjay@gmail.com>
2022-08-17 12:37:00 +0530

    Third Commit

--- src/mydsframework/absClass/MapBody.java
+++ src/mydsframework/absClass/MapBody.java
@@ -21,7 +21,6 @@ public abstract class MapBody<K,T> extends MapNode<K,T> implements Mapping<K,T> 
             hea.next=e;
     }
 
-    //working on algorithm
     public void addAtFront(K key,T value){
 
     }
@@ -29,15 +28,57 @@ public abstract class MapBody<K,T> extends MapNode<K,T> implements Mapping<K,T> 
 
     }
     public T get(K key){
+        NODE temp = this.head.next;
+        T x=null;
+        while(temp.next!=null )
+        {
+            if(temp.key==key){
+                x = temp.value;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(x!=null)
+            return x;
+
         return null;
     }
     public T set(K key,T value){
-        return null;
+        NODE temp = this.head.next;
+        T x=null;
+        while(temp.next!=null )
+        {
+            if(temp.key==key){
+                x = temp.value;
+                temp.value=value;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(x!=null)
+            return x;
+        this.add(key, value);
+        return value;
     }
     public T deleteAtFront(){
         return null;
     }
     public T deleteAtBack(){
         return null;
+    }
+    public T remove(K key){
+        NODE e = this.head;
+        T x = null;
+        while(e.next!=null)
+        {
+            if(e.next.key==key)
+            {
+                x = e.next.value;
+                e.next=e.next.next;
+                break;
+            }
+            e=e.next;
+        }
+        return x;
     }
 }
--- src/mydsframework/in/Mapping.java
+++ src/mydsframework/in/Mapping.java
@@ -2,10 +2,8 @@ package mydsframework.in;
 
 public interface Mapping<K,T> extends DSFramework {
     void add(K key,T value);
-    void addAtFront(K key,T value);
-    void addAtLast(K key,T value);
     T get(K key);
     T set(K key,T value);
-    T deleteAtFront();
-    T deleteAtBack();
+    T remove(K key);
+
 }
